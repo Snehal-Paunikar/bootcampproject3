a9bf46e05f8c4625d3a54df34f8ca543a5644b7d
Snehal-Paunikar <er.snehal.paunikar@outlook.com>
2025-07-17 18:29:58 -0400

    Remove backend-production.tf from staging branch

--- backend-production.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tf-rg"
-    storage_account_name = "tfstorageacctsnehal"
-    container_name       = "tfstate"
-    key                  = "production.tfstate"
-  }
-}
